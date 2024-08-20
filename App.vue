8e91feae2d12b7fb63415539fbed6fcdba97ab49
Gabriel Arruda <108347889+ArrudaG@users.noreply.github.com>
2024-08-19 21:07:46 -0300

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,15 @@
+<script>
+import TaskListApp from './components/TaskListApp.vue'
+export default {
+  name: 'App',
+  components: {
+    TaskListApp
+  }
+}
+</script>
+
+<template>
+  <div id="app">
+    <TaskListApp></TaskListApp>
+  </div>
+</template>
